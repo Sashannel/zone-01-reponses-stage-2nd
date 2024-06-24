621bb8d1d22ecc8ef4428dae0528d4b739045439
Sashannel <143262238+Sashannel@users.noreply.github.com>
2024-06-24 09:28:20 +0200

    Create count-from-10.js

---
+++ 24-06-24/count-from-10.js
@@ -0,0 +1,11 @@
+function from10(end) {
+  if (end < 10) {
+    for (let i = 10; i >= end; i--) {
+      console.log(i)
+    }
+  } else {
+    for (let i = 10; i <= end; i++) {
+      console.log(i)
+    }
+  }
+}
